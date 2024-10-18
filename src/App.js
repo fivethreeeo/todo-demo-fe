2b9b3c2610648063abdd10a741d91aadf8453d76
Sangbeom Heo <sangbeomheo@icloud.com>
2024-10-19 00:06:40 +0900

    프론트 가져오기

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import api from './utils/api'
+import TodoBoard from './components/TodoBoard'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+
+function App() {
+  const [todoList, setTodoList] = useState([])
+  const [todoValue, setTodoValue] = useState('')
+
+  const getTasks = async () => {
+    const response = await api.get('/tasks')
+    setTodoList(response.data.data)
+  }
+
+  const addTask = async () => {
+    try {
+      const response = await api.post('/tasks', {
+        task: todoValue,
+        isCompleted: false,
+      })
+      if (response.status === 200) {
+        setTodoValue('')
+        getTasks()
+        return
+      }
+      throw new Error('할일 추가 실패')
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    getTasks()
+  }, [])
+
+  return (
+    <Container>
+      <Row className='add-item-row'>
+        <Col xs={12} sm={10}>
+          <input
+            type='text'
+            placeholder='할일을 입력하세요'
+            className='input-box'
+            value={todoValue}
+            onChange={event => setTodoValue(event.target.value)}
+          />
+        </Col>
+        <Col xs={12} sm={2}>
+          <button className='button-add' onClick={addTask}>
+            추가
+          </button>
+        </Col>
+      </Row>
+
+      <TodoBoard todoList={todoList} />
+    </Container>
+  )
+}
+
+export default App
---
+++ src/components/TodoItem.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+
+const TodoItem = ({ item }) => {
+  const { id, task, isCompleted } = item
+
+  return (
+    <Row>
+      <Col xs={12}>
+        <div className={`todo-item`}>
+          <div className='todo-content'>{task}</div>
+
+          <div>
+            <button className='button-delete'>삭제</button>
+            <button className='button-delete'>끝남</button>
+          </div>
+        </div>
+      </Col>
+    </Row>
+  )
+}
+
+export default TodoItem
